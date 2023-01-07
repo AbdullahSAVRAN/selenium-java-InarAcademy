ed18c4be4de83b8620f4c3796250f821b08aba2b
AbdullahSAVRAN <111440438+AbdullahSAVRAN@users.noreply.github.com>
2023-01-07 17:32:54 +0300

    Add files via upload

---
+++ selenium_project/src/selenium/java/practice_03/StandAloneTest.java
@@ -0,0 +1,50 @@
+package practice_03;
+
+import example.pageobjects.*;
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.support.ui.ExpectedCondition;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.Assert;
+
+import java.time.Duration;
+import java.util.List;
+
+public class StandAloneTest {
+    public static void main(String[] args) {
+
+
+        String productName = "ZARA COAT 3";
+        WebDriverManager.chromedriver().setup();
+        WebDriver driver = new ChromeDriver();
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+        driver.manage().window().maximize();
+        LandingPage landingPage = new LandingPage(driver);
+        landingPage.goTo();
+        ProductCatalogue productCatalogue = landingPage.loginApplication("anshika@gmail.com","Iamking@000");
+        List<WebElement> products = productCatalogue.getProductList();
+        productCatalogue.addProductToCart(productName);
+        CartPage cartPage = productCatalogue.goToCartPage();
+        Boolean match = cartPage.VerifyProductDisplay(productName);
+        Assert.assertTrue(match);
+        CheckoutPage checkoutPage = cartPage.goToCheckout();
+        checkoutPage.selectCounty("india");
+       ConfirmationPage confirmationPage =  checkoutPage.submitOrder();
+       String confirmMessage = confirmationPage.getConfirmationMessage();
+       Assert.assertTrue(confirmMessage.equalsIgnoreCase("THANKYOU FOR THE ORDER."));
+       driver.close();
+
+
+
+
+
+
+
+
+    }
+}
